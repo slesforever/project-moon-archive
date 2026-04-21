1c7e463ded224ca1b3fa30862551f04adb2a6781
slesforever <sles.forever@gmail.com>
2026-04-21 20:46:44 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,57 @@
 const MASTER_DB = {
-    sinners: [
-        { name: "Yi Sang", tag: "Limbus", desc: "No. 01。前九人會成員。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/a/a2/Yi_Sang_Portrait.png" },
-        { name: "Faust", tag: "Limbus", desc: "No. 02。梅菲斯特號製造者。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/d/d4/Faust_Portrait.png" },
-        { name: "Don Quixote", tag: "Limbus", desc: "No. 03。正義的狂熱信徒。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/0/08/Don_Quixote_Portrait.png" }
-        // ...以此類推，你可以自己新增
-    ],
-    sephirah: [
-        { name: "Angela", tag: "Director", desc: "圖書館館長。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/c/c5/Angela_Portrait.png" }
-    ],
-    enemies: [
-        { name: "Argalia", tag: "Ensemble", desc: "漆黑樂團團長。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/3/3d/Argalia_Portrait.png" }
-    ],
-    abnos: [
-        { name: "Nothing There", tag: "ALEPH", desc: "模仿人類皮囊的怪物。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/1/18/Nothing_There_Breach.png" }
-    ]
+    // --- 第一個作品：Limbus Company ---
+    "LimbusCompany": {
+        "title": "林imbus公司",
+        "categories": {
+            "sinners": {
+                "label": "12 罪人",
+                "items": [
+                    { name: "Yi Sang (李箱)", desc: "No. 01 - 前九人會成員。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/a/a2/Yi_Sang_Portrait.png" },
+                    { name: "Faust (浮士德)", desc: "No. 02 - 掌握都市知識的智者。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/d/d4/Faust_Portrait.png" },
+                    { name: "Don Quixote (唐吉訶德)", desc: "No. 03 - 追求正義的狂熱者。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/0/08/Don_Quixote_Portrait.png" }
+                    // ... 這裡繼續補完 12 罪人
+                ]
+            },
+            "enemies": {
+                "label": "收割者與敵人",
+                "items": [
+                    { name: "Vergilius (維吉爾)", desc: "紅之凝視。導航員。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/a/a2/Vergilius_Portrait.png" }
+                ]
+            }
+        }
+    },
+
+    // --- 第二個作品：Library of Ruina ---
+    "LibraryOfRuina": {
+        "title": "廢墟圖書館",
+        "categories": {
+            "librarians": {
+                "label": "總司書與司書",
+                "items": [
+                    { name: "Angela (安潔拉)", desc: "圖書館館長。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/c/c5/Angela_Portrait.png" },
+                    { name: "Roland (羅蘭)", desc: "黑色沉默。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/9/9d/Roland_Portrait.png" }
+                ]
+            },
+            "ensemble": {
+                "label": "殘響樂團",
+                "items": [
+                    { name: "Argalia (阿爾加利亞)", desc: "漆黑樂團團長。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/3/3d/Argalia_Portrait.png" }
+                ]
+            }
+        }
+    },
+
+    // --- 第三個作品：Lobotomy Corp ---
+    "LobotomyCorp": {
+        "title": "腦葉公司",
+        "categories": {
+            "abnos": {
+                "label": "異想體圖鑑",
+                "items": [
+                    { name: "Nothing There", desc: "ALEPH 級。渴望成為人類。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/1/18/Nothing_There_Breach.png" },
+                    { name: "WhiteNight", desc: "ALEPH 級。救世主。", img: "https://static.wikia.nocookie.net/lobotomycorp/images/b/b3/WhiteNight_Portrait.png" }
+                ]
+            }
+        }
+    }
 };
